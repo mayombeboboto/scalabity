6ffe9d454787617c8a8bea9a7da1a63a0db46017
mayombeboboto <mayombegradi@gmail.com>
2023-07-26 17:11:07 +0200

    Proc_lib modules

--- Chap10/mutex_v1/src/mutex.erl
+++ Chap10/mutex_v1/src/mutex.erl
@@ -6,6 +6,9 @@
 -export([wait/1]).
 -export([signal/1]).
 
+-export([system_continue/3]).
+-export([system_terminate/4]).
+
 -export([init/3]).
 
 %%% APIs
@@ -34,20 +37,60 @@ init(Parent, Name, DbgOpts) ->
     process_flag(trap_exit, true),
     Debug = sys:debug_options(DbgOpts),
     proc_lib:init_ack({ok, self()}),
-    free(Name, Parent, Debug).
+    NewDebug = sys:handle_debug(Debug, fun debug/3, Name, init),
+    free(Name, Parent, NewDebug).
 
 %%% Internal Functions
 free(Name, Parent, Debug) ->
     receive
         {wait, Pid} ->
+            NewDebug = sys:handle_debug(Debug, fun debug/3, Name, {wait, Pid}),
             Pid ! {self(), ok},
-            busy(Pid, Name, Parent, Debug);
+            busy(Pid, Name, Parent, NewDebug);
+        {system, From, Msg} ->
+            sys:handle_system_msg(Msg, From, Parent, ?MODULE, Debug, {free, Name});
         stop ->
-            ok
+            terminate(shutdown, Name, Debug);
+        {'EXIT', Parent, Reason} ->
+            terminate(Reason, Name, Debug)
     end.
 
 busy(Pid, Name, Parent, Debug) ->
     receive
         {signal, Pid} ->
-            free(Name, Parent, Debug)
+            NewDebug = sys:handle_debug(Debug, fun debug/3, Name, {signal, Pid}),
+            free(Name, Parent, NewDebug);
+        {system, From, Msg} ->
+            sys:handle_system_msg(Msg, From, Parent, ?MODULE, Debug, {busy, Name, Pid});
+        {'EXIT', Parent, Reason} ->
+            exit(Pid, Reason),
+            terminate(Reason, Name, Debug)
+    end.
+
+debug(Dev, Event, Name) ->
+    io:format(Dev, "mutex ~w: ~w~n", [Name, Event]).
+
+system_continue(Parent, Debug, {busy, Name, Pid}) ->
+    busy(Pid, Name, Parent, Debug);
+system_continue(Parent, Debug, {free, Name}) ->
+    free(Name, Parent, Debug).
+
+system_terminate(Reason, _Parent, Debug, {busy, Name, Pid}) ->
+    exit(Pid, Reason),
+    terminate(Reason, Name, Debug);
+system_terminate(Reason, _Parent, Debug, {free, Name}) ->
+    terminate(Reason, Name, Debug).
+
+terminate(Reason, Name, Debug) ->
+    unregister(Name),
+    sys:handle_debug(Debug, fun debug/3, Name, {terminate, Reason}),
+    terminate(Reason).
+
+terminate(Reason) ->
+    receive
+        {wait,Pid} ->
+            exit(Pid, Reason),
+            terminate(Reason)
+        after 0 ->
+            exit(Reason)
     end.
